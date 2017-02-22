5eb3f5227114e92f9f6d3695d943dcc327a3c1cf
FoolsLynx <theblacknekos@gmail.com>
2017-02-22 00:24:24 +0000

    Add Tutorial 4 Items
    Added Pickaxe, Axe, Hammer, Hamaxe source code and images.

---
+++ Items/Tools/TutorialPickaxe.cs
@@ -0,0 +1,38 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace TutorialMod.Items.Tools
+{
+    public class TutorialPickaxe : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.name = "Tutorial Pickaxe"; // Name of the Item
+            item.damage = 15; // Base Damage of the Weapon
+            item.width = 24; // Hitbox Width
+            item.height = 24; // Hitbox Height
+            item.toolTip = "This is a modded Pickaxe."; // First Tool Tip
+            item.useTime = 20; // Speed before reuse
+            item.useAnimation = 20; // Animation Speed
+            item.useStyle = 1; // 1 = Broadsword 
+            item.knockBack = 1.5f; // Weapon Knockbase: Higher means greater "launch" distance
+            item.value = 5500; // 10 | 00 | 00 | 00 : Platinum | Gold | Silver | Bronze
+            item.rare = 2; // Item Tier
+            item.UseSound = SoundID.Item1; // Sound effect of item on use 
+            item.autoReuse = true; // Do you want to torture people with clicking? Set to false
+
+            item.pick = 75; // Pick Power - Higher Value = Better
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "TutorialBar", 12);
+            recipe.AddIngredient(ItemID.Wood, 4);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
