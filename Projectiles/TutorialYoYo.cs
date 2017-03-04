6e0fe20b5e1b51b4ce52f53a6dfede4742a9cc5f
FoolsLynx <theblacknekos@gmail.com>
2017-03-04 22:24:40 +0000

    Added Tutorial Spear and YoYo
    Added the Tutorial Spear and YoYo Classes and Images. These are from Episode 5 Part 1

---
+++ Projectiles/TutorialYoYo.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace TutorialMod.Projectiles
+{
+    public class TutorialYoYo : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.name = "Projectile Tutorial YoYo";
+            projectile.width = 16;
+            projectile.height = 16;
+            projectile.aiStyle = 99; // All Yoyos use the style 99.
+            projectile.friendly = true; // Doesn't harm NPCs
+            projectile.penetrate = -1; // -1 = Will not go through enemy.
+            projectile.melee = true; // The projectile is a melee projectile.
+            projectile.scale = 1f; // The scale of the projectile. 2f is double size. 0.5f is half size.
+            ProjectileID.Sets.YoyosLifeTimeMultiplier[projectile.type] = 3.5f;
+            ProjectileID.Sets.YoyosMaximumRange[projectile.type] = 300f;
+            ProjectileID.Sets.YoyosTopSpeed[projectile.type] = 13f;
+        }
+    }
+}
