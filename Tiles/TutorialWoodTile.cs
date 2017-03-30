7f9d9155a10cc64baf393a40bdc12dd3d57bbb68
FoolsLynx <FoolsLynx@users.noreply.github.com>
2017-03-31 00:14:50 +0100

    Add files via upload

---
+++ Tiles/TutorialWoodTile.cs
@@ -0,0 +1,21 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TutorialMod.Tiles
+{
+    public class TutorialWoodTile : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolid[Type] = true; // Is the tile solid
+            Main.tileMergeDirt[Type] = true; // Will tile merge with dirt?
+            Main.tileLighted[Type] = true; // ???
+            drop = mod.ItemType("TutorialWood"); // What item drops after destorying the tile
+            AddMapEntry(new Color(110, 160, 190)); // Colour of Tile on Map
+        }
+        
+    }
+}
