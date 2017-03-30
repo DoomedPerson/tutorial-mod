c82426c991f3fd02466f5d1cafb588a96a71f03a
FoolsLynx <FoolsLynx@users.noreply.github.com>
2017-03-31 00:14:21 +0100

    Add files via upload

---
+++ Walls/TutorialWoodWall.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+
+namespace TutorialMod.Walls
+{
+    public class TutorialWoodWall : ModWall // ModWall is needed for all wall tiles
+    {
+        public override void SetDefaults()
+        {
+            Main.wallHouse[Type] = true; // This wall can be used for a house;
+            drop = mod.ItemType("TutorialWoodWall");
+            AddMapEntry(new Color(110, 160, 190));
+        }
+    }
+}
