d6fa500e4be1ac11bd3a64a1154a15524a247065
FoolsLynx <FoolsLynx@users.noreply.github.com>
2017-04-17 20:30:34 +0100

    Added Tutorial Player
    Forgot to add this ages ago, but has been added now.

---
+++ TutorialPlayer.cs
@@ -0,0 +1,20 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using System.Collections.Generic;
+
+namespace TutorialMod
+{
+    public class TutorialPlayer : ModPlayer
+    {
+        public override void SetupStartInventory(IList<Item> items)
+        {
+            items.Clear();
+
+            Item item = new Item();
+            item.SetDefaults(mod.ItemType("TutorialWood"));
+            item.stack = 5;
+            items.Add(item);
+        }
+    }
+}
