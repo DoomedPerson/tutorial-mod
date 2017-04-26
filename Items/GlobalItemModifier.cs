6d94580ceab902e84798c12c531a748fcae2984d
FoolsLynx <FoolsLynx@users.noreply.github.com>
2017-04-26 12:16:58 +0100

    Add files via upload

---
+++ Items/GlobalItemModifier.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TutorialMod.Items
+{
+    public class GlobalItemModifier : GlobalItem
+    {
+        public override void SetDefaults(Item item)
+        {
+            if(item.type == ItemID.CopperShortsword)
+            {
+                item.damage = 100;
+            }
+
+            if(item.type == ItemID.CopperPickaxe)
+            {
+                item.pick = 200;
+                item.useTime = 5;
+                item.useAnimation = 10;
+            }
+        }
+    }
+}
