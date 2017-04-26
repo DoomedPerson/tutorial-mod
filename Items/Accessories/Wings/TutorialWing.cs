c5695d440ed7952c2d86f6aa20c990e355d3f1f5
FoolsLynx <FoolsLynx@users.noreply.github.com>
2017-04-26 12:19:26 +0100

    Add files via upload

---
+++ Items/Accessories/Wings/TutorialWing.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TutorialMod.Items.Accessories.Wings
+{
+    public class TutorialWing : ModItem
+    {
+        public override bool Autoload(ref string name, ref string texture, IList<EquipType> equips)
+        {
+            equips.Add(EquipType.Wings);
+            return true;
+        }
+
+        public override void SetDefaults()
+        {
+            item.name = "Tutorial Wings";
+            item.width = 22;
+            item.height = 22;
+            item.value = 10000;
+            item.rare = 4;
+            item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.wingTimeMax = 250;
+        }
+
+        public override void VerticalWingSpeeds(Player player, ref float ascentWhenFalling, ref float ascentWhenRising, ref float maxCanAscendMultiplier, ref float maxAscentMultiplier, ref float constantAscend)
+        {
+            ascentWhenFalling = 0.9f;
+            ascentWhenRising = 0.1f;
+            maxCanAscendMultiplier = 1.2f;
+            maxAscentMultiplier = 3.5f;
+            constantAscend = 0.125f;
+        }
+
+        public override void HorizontalWingSpeeds(Player player, ref float speed, ref float acceleration)
+        {
+            speed = 10f;
+            acceleration *= 2.75f;
+        }
+
+    }
+
+
+}
