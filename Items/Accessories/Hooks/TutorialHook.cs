f69f066ef66fc3d9327fa9f839ad6e06defbf927
FoolsLynx <FoolsLynx@users.noreply.github.com>
2017-04-26 12:17:45 +0100

    Add files via upload

---
+++ Items/Accessories/Hooks/TutorialHook.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace TutorialMod.Items.Accessories.Hooks
+{
+    public class TutorialHook : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.CloneDefaults(ItemID.AmethystHook);
+            item.name = "Tutorial Hook";
+            item.shootSpeed = 18f;
+            item.shoot = mod.ProjectileType("TutorialHook");
+        }
+    }
+}
