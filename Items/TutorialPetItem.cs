e0fdc119a2431cd2e6112be671af1a5092383e86
FoolsLynx <FoolsLynx@users.noreply.github.com>
2017-05-25 15:30:22 +0100

    Added Tutorial Pet Item (Episode 11)

---
+++ Items/TutorialPetItem.cs
@@ -0,0 +1,35 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace TutorialMod.Items
+{
+    public class TutorialPetItem : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.CloneDefaults(ItemID.ZephyrFish);  // Clone the defaults of Zephyr Fish
+            item.name = "Blocky";
+            item.toolTip = "Summons Blocky";
+            item.shoot = mod.ProjectileType("TutorialPet");
+            item.buffType = mod.BuffType("TutorialPetBuff");
+        }
+
+        public override void UseStyle(Player player)
+        {
+            if(player.whoAmI == Main.myPlayer && player.itemTime == 0)
+            {
+                player.AddBuff(item.buffType, 3600, true);
+            }
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe r = new ModRecipe(mod);
+            r.AddIngredient(null, "TutorialWood", 5);
+            r.AddTile(null, "TutorialWorkbench");
+            r.SetResult(this);
+            r.AddRecipe();
+        }
+    }
+}
