e24331355d8d9807061327ccb46e53d27afad17a
FoolsLynx <FoolsLynx@users.noreply.github.com>
2017-05-25 15:34:54 +0100

    TutorialBiomeBlock and Tutorial Workbench

---
+++ Items/Placeable/TutorialBiomeBlock.cs
@@ -0,0 +1,26 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace TutorialMod.Items.Placeable
+{
+    public class TutorialBiomeBlock : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.name = "Biome Block"; 
+            item.width = 12; 
+            item.height = 12;
+            item.useTime = 20; 
+            item.useAnimation = 20;
+            item.useStyle = 1; 
+            item.value = 50; 
+            item.rare = 2;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true; 
+            item.consumable = true; 
+            item.createTile = mod.TileType("TutorialBiomeTile");
+            item.maxStack = 999; 
+        }
+    }
+}
---
+++ Items/Placeable/TutorialWorkbench.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace TutorialMod.Items.Placeable
+{
+    public class TutorialWorkbench : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.name = "Tutorial Workbench";
+            item.width = 12;
+            item.height = 12;
+            item.useTime = 14;
+            item.useAnimation = 17;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useStyle = 1;
+            item.createTile = mod.TileType("TutorialWorkbench");
+            item.consumable = true;
+        }
+    }
+}
