32092322016903458ab2819ab4cbd11ac860a742
FoolsLynx <FoolsLynx@users.noreply.github.com>
2017-05-25 15:33:52 +0100

    Added Tutorial Workbench and Biome Tile

---
+++ Tiles/TutorialWorkbench.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.ObjectData;
+using Microsoft.Xna.Framework;
+
+namespace TutorialMod.Tiles
+{
+    public class TutorialWorkbench : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolidTop[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileTable[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x1);
+            TileObjectData.newTile.CoordinateHeights = new int[] { 18 };
+            TileObjectData.addTile(Type);
+            AddMapEntry(new Color(200, 200, 200), "Tutorial Workbench");
+            adjTiles = new int[] { TileID.WorkBenches };
+            disableSmartCursor = true;
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(i * 16, j * 16, 32, 16, mod.ItemType("TutorialWorkbench"));
+        }
+    }
+}
