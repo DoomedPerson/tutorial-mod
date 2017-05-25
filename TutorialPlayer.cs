90d64434f7873328dd0f514087b5f077bd597e4e
FoolsLynx <FoolsLynx@users.noreply.github.com>
2017-05-25 15:32:48 +0100

    Updated TutorialPlayer for Biome and Pet

--- TutorialPlayer.cs
+++ TutorialPlayer.cs
@@ -1,12 +1,23 @@
-﻿using Terraria;
+using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
 using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace TutorialMod
 {
     public class TutorialPlayer : ModPlayer
     {
+        public bool tutorialPet = false;
+
+        public bool zoneBiome = false;
+
+        public override void ResetEffects()
+        {
+            tutorialPet = false;
+        }
+
         public override void SetupStartInventory(IList<Item> items)
         {
             items.Clear();
@@ -16,5 +27,47 @@ namespace TutorialMod
             item.stack = 5;
             items.Add(item);
         }
+
+        public override void UpdateBiomes()
+        {
+            zoneBiome = (TutorialWorld.biomeTiles > 50); // Chance 50 to the minimum number of tiles that need to be counted before it is classed as a biome
+        }
+
+        public override bool CustomBiomesMatch(Player other)
+        {
+            TutorialPlayer otherPlayer = other.GetModPlayer<TutorialPlayer>(mod); // This will get other players using the TutorialPlayerClass
+            return zoneBiome == otherPlayer.zoneBiome; // This will return true or false depending on other player
+        }
+
+        public override void CopyCustomBiomesTo(Player other)
+        {
+            TutorialPlayer otherPlayer = other.GetModPlayer<TutorialPlayer>(mod);
+            otherPlayer.zoneBiome = zoneBiome; // This will set other player's biome to the same as thisPlayer
+        }
+
+        public override void SendCustomBiomes(BinaryWriter writer)
+        {
+            BitsByte flags = new BitsByte();
+            flags[0] = zoneBiome;
+            writer.Write(flags);
+        }
+
+        public override void ReceiveCustomBiomes(BinaryReader reader)
+        {
+            BitsByte flags = reader.ReadByte();
+            zoneBiome = flags[0];
+        }
+
+        public override void UpdateBiomeVisuals()
+        {
+            
+        }
+
+        public override Texture2D GetMapBackgroundImage()
+        {
+            return null;
+        }
+
+
     }
 }
