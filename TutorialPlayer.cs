bb862835cc8193391ae5f673de0528553076db36
FoolsLynx <FoolsLynx@users.noreply.github.com>
2017-08-04 19:17:42 +0100

    Add files via upload

--- TutorialPlayer.cs
+++ TutorialPlayer.cs
@@ -1,4 +1,4 @@
-using Terraria;
+﻿using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
 using System.Collections.Generic;
--- TutorialWorld.cs
+++ TutorialWorld.cs
@@ -1,4 +1,4 @@
-using Terraria;
+﻿using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.GameContent.Generation;
@@ -6,12 +6,61 @@ using Terraria.World.Generation;
 using System.Collections.Generic;
 
 using System;
+using Terraria.ModLoader.IO;
+using System.IO;
 
 namespace TutorialMod
 {
     public class TutorialWorld : ModWorld
     {
         public static int biomeTiles = 0;
+        // Stuff added with the Boss
+        public static bool downedTutorialBoss = false; // Downed Tutorial Boss
+
+        public override void Initialize()
+        {
+            downedTutorialBoss = false;
+        }
+
+        public override TagCompound Save()
+        {
+            var downed = new List<string>();
+            if (downedTutorialBoss) downed.Add("tutorial");
+
+            return new TagCompound
+            {
+                {"downed", downed }
+            };
+        }
+
+        public override void Load(TagCompound tag)
+        {
+            var downed = tag.GetList<string>("downed");
+            downedTutorialBoss = downed.Contains("tutorial");
+        }
+
+        public override void LoadLegacy(BinaryReader reader)
+        {
+            int loadVersion = reader.ReadInt32();
+            if(loadVersion == 0)
+            {
+                BitsByte flags = reader.ReadByte();
+                downedTutorialBoss = flags[0];
+            }
+        }
+
+        public override void NetSend(BinaryWriter writer)
+        {
+            BitsByte flags = new BitsByte();
+            flags[0] = downedTutorialBoss;
+            writer.Write(flags);
+        }
+
+        public override void NetReceive(BinaryReader reader)
+        {
+            BitsByte flags = reader.ReadByte();
+            downedTutorialBoss = flags[0];
+        }
 
         public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight)
         {
