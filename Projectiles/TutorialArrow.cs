ce6a143c4d84f82bfe740d3a6ae450f97d501d88
FoolsLynx <FoolsLynx@users.noreply.github.com>
2017-10-12 15:13:35 +0100

    Add files via upload

---
+++ Projectiles/TutorialArrow.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace TutorialMod.Projectiles
+{
+    public class TutorialArrow : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Tutorial Arrow");
+            ProjectileID.Sets.TrailCacheLength[projectile.type] = 5;
+            ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 8;
+            projectile.height = 8;
+            projectile.aiStyle = 1;
+            projectile.friendly = true;         
+            projectile.hostile = false;         
+            projectile.ranged = true;           
+            projectile.penetrate = 5;           
+            projectile.timeLeft = 600;           
+            projectile.light = 0.5f;            
+            projectile.ignoreWater = true;      
+            projectile.tileCollide = true;      
+            projectile.extraUpdates = 1;                                           
+            aiType = ProjectileID.WoodenArrowFriendly; 
+        }
+    }
+}
